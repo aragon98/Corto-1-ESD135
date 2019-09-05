8035160821c7c59263c00555de982e67628439b4
aragon98 <48097234+aragon98@users.noreply.github.com>
2019-09-05 14:21:06 -0600

    Add files via upload

--- ejercicio#1.c
+++ ejercicio#1.c
@@ -1,22 +1,27 @@
 #include<stdio.h>
-#define size 3
+#define size 5
 int main()
 {
-int sala[size][size]={{1, 2, 3},{4, 5, 6},{7, 8, 9}};
+int sala[size][size]={{1, 2, 3, 4, 5},{6, 7, 8, 9, 10},{11, 12, 13, 14, 15},{16, 17, 18, 19, 20},{21, 22, 23, 24, 25}};
 
 	
 
-float ganancia=0;
+float ganancia=0, aux, costo;
+
 int opcion, asiento, i, j; 
 
 do{
+	
 	//Creamos un menu y mostramos la matriz 0 equivale a asiento vacio y 1 a ocupado
-	printf("Bienvenido a Cinepolis\n");
+	printf("\nBienvenido a Cinepolis\n");
 	printf("Menu: (Seleccione un numero)\n");
-	printf("1. Seleccione un asiento\n");
+	printf("1. Seleccionar asiento.\n");
+	printf("2. Ver ganancia\n");
+	printf("3. Salir\n");
 	scanf("%d",&opcion);
 	switch(opcion){
 	case 1:
+		//Mostramos asientos al usuario
 		for (i = 0; i <size ; i++)
 		{
 			printf("\n");
@@ -26,21 +31,59 @@ do{
 			}
 			
 		}
-		printf("¿Que asiento desea?");
+		//Selecion del usuario
+		printf("\n¿Que asiento desea?\n");
 		scanf("%d",&asiento);
-		
-		//Con 0 pondremos el asiento ocupado
-		for (i = 0; i < ; i++)
+		//Validamos si se ingresa numero negativo o mayor al de los asientos
+		if (asiento<0 || asiento>25)
 		{
-			for (j = 0; j < ; j++)
+			printf("El asiento no existe");
+		}else{
+		
+	for (i = 0; i < size; i++)
+	{
+		if (asiento==sala[0][i])
+		{
+			costo=5.0;
+		}else{
+			if (asiento==sala[4][i])
+			{
+				costo=2.5;
+			}else
+			{
+				if (asiento!=sala[0][i] && asiento!=sala[4][i] )
+						{
+							costo=3.5;
+						}
+			}
+		}
+	}
+	
+	//Con 0 pondremos el asiento ocupado y validamos si se ingresa un 0
+		for (i = 0; i <size; i++)
+		{
+			for (j = 0; j < size; j++)
 			{
 				if(asiento==sala[i][j])
 				{
 						sala[i][j]=0;
+				}else
+				{
+					
 				}
-			}
-			
+			}		
 		}
+		if (asiento==0)
+					{
+						printf("\nAsiento ocupado\n");
+					}		
+	
+		
+		
+		//Acumulamos la ganancia en una variable
+		ganancia=ganancia+costo;
+		
+		//Imprimimos
 		for (i = 0; i <size ; i++)
 		{
 			printf("\n");
@@ -48,13 +91,24 @@ do{
 			{
 				printf("%d\t",sala[i][j]);
 			}
+		}
 		
-		
-		
+		printf("\nEl costo es de $%f",costo);
+		printf("\nEn el asiento %d",asiento);
+	}
+		printf("\n");	
+	break;
+	
+	//Ver ganancia
+	case 2:
+		printf("\nLa ganancia acumulada es de %f",ganancia);
 	break;
 		
 	}
-	}while(opcion !=2);
+	}while(opcion==1 || opcion==2);
+	
+	printf("\n");
+	printf("Hasta la proxima xD\n");
 
 return 0;
 }
